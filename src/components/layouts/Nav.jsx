ac1d2585ab39ae53549241a0dd59bbdc6cf6a692
HkWolf <hkwolfx@gmail.com>
2019-10-25 14:38:27 +0200

    Leviosound Vanilla

---
+++ src/components/layouts/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Nav.css'
+
+
+
+export default class Nav extends React.Component {
+
+
+
+    render() {
+        return (
+            <div className="container">
+                <p className="Title">Leviosound</p>
+            </div>
+
+        )
+    }
+}
+
+
+
+
+
